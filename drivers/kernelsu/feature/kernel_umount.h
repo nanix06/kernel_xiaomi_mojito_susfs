07e332cb9bf45675ddb07c6711569940a35aa576
backslashxx <118538522+backslashxx@users.noreply.github.com>
2026-05-04 19:36:10 +0800

    KernelSU: import 32582

---
+++ drivers/kernelsu/feature/kernel_umount.h
@@ -0,0 +1,15 @@
+#ifndef __KSU_H_KERNEL_UMOUNT
+#define __KSU_H_KERNEL_UMOUNT
+
+// for the umount list
+struct mount_entry {
+    char *umountable;
+    unsigned int flags;
+    struct list_head list;
+};
+extern struct list_head mount_list;
+extern struct rw_semaphore mount_list_lock;
+
+bool ksu_is_webview_zygote_umount_enabled(void);
+
+#endif
